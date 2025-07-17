9f441602ae58e3156992c294be2ab599c16622d0
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-17 20:48:54 +0300

    Upload Project

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,103 @@ arr5.push(20); // No Error
 const fun: (x: number, y: number) => string = (x, y) => {
   return (x + y).toString();
 };
+
+// Interface
+interface Person {
+  name: string;
+  age: number;
+}
+const xxxx: Person = {
+  name: "John",
+  age: 30,
+};
+
+// Type Alias
+type MyType = string | number | boolean;
+const yyyy2: MyType = "Hello";
+const yyyy3: MyType = 1;
+const yyyy4: MyType = true;
+
+type Person2 = {
+  name: string;
+  age: number;
+};
+const yyyy: Person2 = {
+  name: "John",
+  age: 30,
+};
+
+console.log(xxxx.name);
+
+// Optional Chaining
+type Person3 = {
+  name: "John",
+  age?: 30,
+};
+const obj: Person3 = {
+  name: "John",
+};
+
+const mynumber: number = obj.age!;
+const mynumber2: number = obj.age ?? 0;
+
+// Enums
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+const mycolor: Color = Color.Red;
+console.log(mycolor);
+let coloring: Color = Color.Red;
+if (coloring === Color.Red) {
+  console.log(Color.Red);
+}
+
+// Classes
+class Car {
+  name: string;
+  year: number;
+  constructor(name: string, year: number) {
+    this.name = name;
+    this.year = year;
+  }
+}
+const c = new Car("Ford", 2020);
+
+// Interface for Class
+interface IVehicle {
+  name: string;
+  year: number;
+}
+class Car2 implements IVehicle {
+  name: string;
+  year: number;
+  constructor(name: string, year: number) {
+    this.name = name;
+    this.year = year;
+  }
+}
+const c2 = new Car2("Ford", 2020);
+
+interface IVehicle2 {
+  name: string;
+  year: number;
+  sayHello: () => void;
+}
+class Car3 implements IVehicle2 {
+  name: string;
+  year: number;
+  constructor(name: string, year: number) {
+    this.name = name;
+    this.year = year;
+  }
+  sayHello() {
+    console.log("Hello");
+  }
+}
+const c3 = new Car3("Ford", 2020);
+
+// Exporting Interfaces
+
+
