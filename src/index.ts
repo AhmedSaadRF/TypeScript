ba4aed798e21b9aa5a8707fac18d06d97f98d803
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-17 19:08:28 +0300

    Upload Project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+var str = "Hello World";
+alert(str);
 
+let arr: number[] = [1, 2, 3, 4, 5];
+let arr2: (string | number | boolean)[] = [1, 2, 3, true, "Hello"];
+let arr3: any[] = [1, 2, 3, true, "Hello"];
+
+let arr4: [string, number] = ["Hello", 1];
+let arr5: (string | number)[] = ["Hello", 1];
+arr4.push(20); // Error
+arr5.push(20); // No Error
+
+const fun: (x: number, y: number) => string = (x, y) => {
+  return (x + y).toString();
+};
