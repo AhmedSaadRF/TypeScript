f5bf21006bf0e8d5fb844a09cef93ad6bce35205
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-18 11:40:42 +0300

    Upload Project

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BookingStatus = void 0;
 var str = "Hello World";
 alert(str);
 let arr = [1, 2, 3, 4, 5];
@@ -42,6 +44,13 @@ let coloring = Color.Red;
 if (coloring === Color.Red) {
     console.log(Color.Red);
 }
+var BookingStatus;
+(function (BookingStatus) {
+    BookingStatus[BookingStatus["PENDING"] = 0] = "PENDING";
+    BookingStatus[BookingStatus["CONFIRMED"] = 1] = "CONFIRMED";
+    BookingStatus[BookingStatus["CANCELLED"] = 2] = "CANCELLED";
+})(BookingStatus || (exports.BookingStatus = BookingStatus = {}));
+const bookingStatus = BookingStatus.CONFIRMED;
 // Classes
 class Car {
     constructor(name, year) {
@@ -67,4 +76,32 @@ class Car3 {
     }
 }
 const c3 = new Car3("Ford", 2020);
-// Exporting Interfaces
+// Generic
+function greet(name) {
+    return `Hello ${name}`;
+}
+console.log(greet("John"));
+greet(1);
+// General Objects Types
+const obj2 = {
+    name: "John",
+    age: 30,
+};
+obj2.username = "Jane";
+const user = {
+    id: "1",
+    name: "John",
+};
+const user2 = {
+    id: "1",
+    name: "John",
+};
+const user3 = {
+    id: "1",
+    name: "John",
+};
+const user4 = {
+    id: "1",
+    name: "John",
+    email: "john@gmail.com",
+};
