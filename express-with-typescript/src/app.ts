3f41c14848a2179fb94841ee1c51059f54630975
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-18 12:10:04 +0300

    Upload Project

--- express-with-typescript/src/app.ts
+++ express-with-typescript/src/app.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', (req: Request, res: Response) => {
+  const x = req.query;
+  res.send(x);
 });
+console.log("Hello, World!");
+
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
